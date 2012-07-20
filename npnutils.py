0040213fd89d712776dd7a7c6b18633dc5cd5d15
nullpass <nullpass@gmail.com>
2012-07-20 15:45:44 -0300

    Update npnutils.py

--- npnutils.py
+++ npnutils.py
@@ -43,3 +43,56 @@ def bark(a):
     return
     
     
+    
+    def require_path(p):
+    """
+    Require a path (diretory or file) and halt execution if it's not
+    present.
+
+    Accepts:
+    p - str. Path to directory or file.
+    
+    Returns:
+    None
+    
+    Outputs:
+    Debug output if debugg is True
+    
+    """
+    #bark("require "+p)
+    if not os.path.exists(p):
+        bark("FATAL Required path \""+p+"\" not found.")
+        sys.exit(10)
+    return
+
+def require_create_path(p):
+    """
+    Require a file or dir, create if missing, halt if can't create.
+    
+    Accepts:
+    p - str. Path to directory or file.
+
+    Returns:
+    None
+
+    Outputs:
+    Debug output if debugg is True
+    
+    """
+    #bark("requireCreate "+p)
+    if not os.path.exists(p):
+        bark("WARNING Required path \""+p+"\" not found, being created.")
+        if p[-1] == "/":
+            try:
+                os.mkdir(p,0700)
+            except:
+                bark("FATAL Failed to create required dir \""+p+"\"")
+                sys.exit(10)
+        else:
+            try:
+                f = open(p,'w')
+                f.close()
+            except:
+                bark("FATAL Failed to create required file \""+p+"\"")
+                sys.exit(10)
+    return

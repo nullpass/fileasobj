24551f3b5233715c1fd918bc5bf5749513ecc300
nullpass <nullpass@gmail.com>
2012-08-11 19:15:30 -0300

    Update bark.py

--- bark.py
+++ bark.py
@@ -31,7 +31,7 @@ bark.do('Hello World.')
 TODO:
     - add main() back and allow strings from sys.argv
 """
-__version__='3.0.b'
+__version__='3.0.1'
 import time
 import os
 from platform import node
@@ -43,7 +43,6 @@ class Bark:
     """
     def __init__(self):
         self.Birthday = (float(time.time()),str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime())))
-        self.logfile = os.path.expanduser('~')+"/log/"+thisExec+".log"
         self.Enabled = True
         #
         # Try to get base name of current program minus the extension
@@ -55,7 +54,7 @@ class Bark:
             self.thisExec = os.path.basename(sys.argv[0])
         #
         # If the name of the executable is too short or None make it py
-        if len(thisExec) < 3:
+        if len(self.thisExec) < 3:
             self.thisExec = 'python'
         #
         # Hostname
@@ -64,6 +63,7 @@ class Bark:
         # Current executable and PID
         self.thisProc = self.thisExec+"["+str(os.getpid())+"]"
         #
+        self.logfile = os.path.expanduser('~')+"/log/"+self.thisExec+".log"
     def do(self,thisEvent):
         if self.Enabled == True:
             if self.logfile:
@@ -71,7 +71,7 @@ class Bark:
                     #
                     # Try to log event to log file that already exists.
                     self.fileHandle = open(self.logfile, 'a')
-                    self.fileHandle.write(str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(self.thisEvent)+'\n')
+                    self.fileHandle.write(str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(thisEvent)+'\n')
                     self.fileHandle.close()
                 except Exception as self.thisError:
                     #
@@ -79,6 +79,6 @@ class Bark:
                     # Sun Aug 05 14:20:37 EDT 2012 myprogram[3125] [Errno 2] No such file or directory: '/home/me/log/myprogram.log'
                     # Sun Aug 05 14:20:37 EDT 2012 myprogram[3125] hello world
                     print str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(self.thisError)
-                    print str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(self.thisEvent)
+                    print str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(thisEvent)
             else:
-                print str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(self.thisEvent)
+                print str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+" "+self.thisProc+" "+str(thisEvent)

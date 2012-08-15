2317a419a934cbb45a251a83c36582c6159d0f69
John <nullpass@gmail.com>
2012-08-15 13:01:19 -0300

    Update FileAsObj.py

--- FileAsObj.py
+++ FileAsObj.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+"""
+FileAsObj.py - Manage a local file as an object. Store contents in a 
+                uniq list and ignore commented lines.
+
+nullpass, 2012
+
+"""
+__version__ = '1.b.0'
+
+import sys
+import os
+import fileinput
+import platform
+import time
+
 class FileAsObj:
     """
     Manage a file as an object. Each line of a file is added to the list
@@ -14,32 +32,68 @@ class FileAsObj:
         read file into list varaible, uniq and without line breaks
         Ignore lines that start with #
         """
+        self.Birthday = (float(time.time()),str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime())))
+        #
+        # Name of file this is running as
+        self.thisExec = str(os.path.basename(sys.argv[0]))
+        if len(self.thisExec) < 3 or len(self.thisExec) > 255:
+            #
+            # If there's no name in argv[0], like if you call this from 
+            # the Python shell, call this python_$$
+            # Also catches names too short or long.
+            self.thisExec = 'python_'+str(os.getpid())
+        #
+        # Hostname
+        self.thisHost = node()
+        #
+        # Current executable and PID, like myapp[12345]
+        self.thisProc = self.thisExec.rstrip('.py')+"["+str(os.getpid())+"]"
+        #
+        # List of any exceptions caught
+        self.Errors = []
+        #
+        # String containing information about steps taken. Used for debugging
+        self.Trace = ''
+        #
+        #
         self.contents = []
-        bark("Read-only opening "+file)
-        for line in fileinput.input(file):
-            if line[0] is not "#":
-                #
-                #uniq the contents of the file when reading.
-                line = line.strip("\n")
-                if len(line) > 1 and line not in self.contents:
-                    self.contents.append(line)
-        fileinput.close()
+        #
+        #
+        try:
+            self.__log('Read-only opening '+file)
+            for line in fileinput.input(file):
+                if line[0] is not "#":
+                    #
+                    #uniq the contents of the file when reading.
+                    line = line.strip("\n")
+                    if len(line) > 1 and line not in self.contents:
+                        self.contents.append(line)
+            fileinput.close()
+        except Exception as e:
+            self.Errors.append(e)
+            return False
         #
         #declare current state is original data from file.
         self.virgin = True
         #
         #set filename inside object, used during .write()
         self.filename = file
-    def isVirgin(self):
+    def __log(self,thisEvent):
         """
-        self.virgin is True or False.
-        True = hasn't changed since __init__
-        False = something was added or removed from object.
+        Private method to update self.Trace with str(thisEvent) given
+        as argument.
         """
-        return self.virgin
+        if not self.Enabled:
+            return True
+        self.Trace += str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+' '+self.thisHost+' '+self.thisProc+' '+str(thisEvent)+'\n'
+        return
     def check(self,needle):
         """
         check existing contents of file for a string
+        
+        if myfile.check('foo'):
+            -or-
+        if 'foo' in myfile.contents:
         """
         if needle in self.contents:
             return True
@@ -47,7 +101,6 @@ class FileAsObj:
     def add(self,item):
         """
         add item to end of list unless it already exists.
-
         """
         #
         #Check for the item.
@@ -68,7 +121,7 @@ class FileAsObj:
         """
         #
         #Check for item
-        bark("Call to remove '"+item+"' from "+self.filename)
+        self.__log("Call to remove '"+item+"' from "+self.filename)
         if item in self.contents:
             #
             #item found, removed.
@@ -92,9 +145,17 @@ class FileAsObj:
         write self.contents to self.filename
         self.filename was defined during __init__
         """
-        bark("Writing "+self.filename)
+        self.__log("Writing "+self.filename)
         f = open(self.filename, "w")
         for line in self.contents:
             f.write(line+"\n")
         f.close()
         return True
+
+def main():
+    
+	return 0
+
+if __name__ == '__main__':
+	main()
+

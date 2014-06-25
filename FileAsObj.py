78872332961cc51027859f4c2ed61029e965ab7e
John <nullpass@gmail.com>
2014-06-25 12:55:04 -0400

    Update FileAsObj.py
    version 2, beta

--- FileAsObj.py
+++ FileAsObj.py
@@ -7,6 +7,7 @@ fileasobj.py - Manage a local file as an object. Store contents in a
 
 nullpass, 2012
 
+2014.06.20 - V2, added [e]grep, dump and verbose; some code correction
 2012.08.15 - Full conversion to portability, added .read()
 2012.07.20 - Initial release
 
@@ -41,29 +42,55 @@ else:
 # contents would then be: ['valid entry', '  # will read()/write() this entry', 'site.ltd', 'foo.bar', '.lol', 'foo', 'bar']
 
 """
-__version__ = '1.0.0'
+__version__ = '2.0.b'
 
 import sys
 import os
 import fileinput
 from platform import node
 import time
+import re
+
 
 class FileAsObj:
     """
-    Manage a file as an object. Each line of a file is added to the list
-    'self.contents'. Lines that start with a # are ignored. Lines that
-    already exist in the .contents lists are ignored- this uniqs the 
-    data, however no sorting is done.
+    Manage a file as an object-
+            -For when you just can't be bothered to use a real database.
+    
+   
+    Each line of a file is added to the list 'self.contents'. 
+    By default lines that start with a # are ignored. 
+    By default data in self.contents is unique.
+    Lines are sorted by whatever order they appear in the file.
     Elements can be added or removed with .add and .rm. 
     The object's contents can be written back to the file, overwritting
     the file, with .write. 
     
+    Error handling: I will not sys.exit or throw an exception. All errors
+        are logged to self.Trace and self.Errors. It is the duty of the 
+        calling code to check for self.Errors and act however is needed.
+        
+        self.Trace is a log of everything the instance of this class
+        did. If there is an exception it is included.
+        
+        self.Errors is a list of exceptions.
+    
     """
-    def __init__(self):
+    
+    def __init__(self,filename=None, verbose=False):
         """
-        read file into list varaible, uniq and without line breaks
-        Ignore lines that start with #
+        
+        You may specify the file to read() during instatiation, but be
+        sure to check for self.Errors. 
+        
+        verbose - BE CAREFUL! if you enable verbose all of the lines in 
+            your file will be added to self.contents. This includes 
+            comments and duplicate lines. If you rely on this classes'
+            .grep() or .egrep() methods be sure you understand that 
+            comments can be returned as a valid result!
+            My greps will return the first match found, in the real-
+            world that is usually a commented entry. 
+        
         """
         self.Birthday = (float(time.time()),str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime())))
         #
@@ -88,18 +115,27 @@ class FileAsObj:
         # String containing information about steps taken. Used for debugging
         self.Trace = ''
         #
+        # If enabled, I will not unique contents or ignore comments.
+        self.verbose = verbose
+        #
         #
         self.contents = []
-        self.filename = None
+        #
+        # If you gave me a file to read when instatiated, then do so.
+        self.filename = filename
+        if self.filename:
+            self.read(self.filename)
         #
         #declare current state is original data from thisFile.
         self.virgin = True
+        
     def __log(self,thisEvent):
         """
         Private method to update self.Trace with str(thisEvent)
         """
         self.Trace += str(time.strftime("%a %b %d %H:%M:%S %Z %Y", time.localtime()))+' '+self.thisHost+' '+self.thisProc+' '+str(thisEvent)+'\n'
         return
+    
     def read(self,thisFile):
         """
         
@@ -113,75 +149,88 @@ class FileAsObj:
         """
         self.filename = str.strip(thisFile)
         try:
-            self.__log('Read-only opening '+str(self.filename))
-            for line in fileinput.input(self.filename):
-                if line[0] is not "#":
-                    #
-                    # uniq the contents of the thisFile when read()ing.
-                    # Ignore lines that have few then 2 characters
-                    line = line.strip("\n")
-                    if len(line) > 1 and line not in self.contents:
-                        self.contents.append(line)
-            fileinput.close()
-            self.__log('Wrote '+str( len(self.contents) )+' lines')
+            self.__log('Read-only opening %s ' % self.filename)
+            with open(self.filename, 'r') as fileHandle:
+				for line in fileHandle:
+					line = line.strip("\n")
+					#
+					# blank lines that were just \n become None, 
+					# so make sure this pass of line exists
+					if line:
+						if self.verbose:
+							#
+							# Some crazy person enabled verbose, just 
+							# add whatever is in the file to 
+							# self.contents.
+							# May whatever god you believe in 
+							#   have mercy on your code.
+							self.contents.append(line)
+						elif line[0] is not "#":
+							# Line is not a comment
+							#
+							#
+							# unique the contents of the thisFile when
+							# read()ing.
+							# Ignore lines that have fewer than 2 
+							# characters
+							if len(line) > 1 and line not in self.contents:
+								self.contents.append(line)
+            self.__log('Read %s lines' % len(self.contents))
             return True
         except Exception as e:
-            self.__log('ERROR during read(self,thisFile)')
+            self.__log('ERROR during read(self,thisFile) : %s' % e)
             self.Errors.append(e)
             return False
+    
     def check(self,needle):
         """
         check existing contents of file for a string
         
-        if myfile.check('foo'):
-            -or-
-        if 'foo' in myfile.contents:
+        This searches each line as a whole, if you want to see if 
+        a substring is in a line, use .grep() or .egrep() methods.
+        
+        If found, return the needle; makes it easier for some code
+        to delete more efficiently. 
         """
         if needle in self.contents:
-            return True
+            return needle
         return False
+        
     def add(self,thisItem):
         """
         add thisItem to end of list unless it already exists.
+        
+        RFC: should we add anyway if verbose == True?
         """
-        #
-        #Check for the item.
+        self.__log('Call to add "%s" to %s' % (thisItem, self.filename) )
         if thisItem not in self.contents:
-            #
-            # not present, adding.
             self.contents.append(thisItem)
-            #
-            #declare something in this object has changed since __init__
             self.virgin = False
             return True
-        #
-        # Already present, no changes made.
         return False
+        
     def rm(self,thisItem):
         """
         remove thisItem from contents.
         """
-        self.__log('Call to remove "'+str(thisItem)+'" from '+str(self.filename))
-        #
-        #Check for item
+        self.__log('Call to remove "%s" from %s' % (thisItem, self.filename) )
         if thisItem in self.contents:
-            #
-            #thisItem found, removed.
             self.contents.remove(thisItem)
-            #
-            #declare something in this object has changed since __init__
             self.virgin = False
             return True
-        #
-        # wasn't there, nothing changed.
         return False
+        
     def inventory(self):
         """
         return contents of self.contents
         
         Useful for printing contents, but hilariously redundant.
+        
+        I keep wanting to take this stupid thing out, but it makes me
+        laugh every time I see it. 
         """
         return self.contents
+        
     def write(self):
         """
         write self.contents to self.filename
@@ -201,12 +250,41 @@ class FileAsObj:
         """
         try:
             self.__log('Writing '+str(self.filename))
-            fileHandle = open(self.filename, 'w')
-            for thisLine in self.contents:
-                fileHandle.write(thisLine+'\n')
-            fileHandle.close()
+            with open(self.filename, 'w') as fileHandle:
+                for thisLine in self.contents:
+                    fileHandle.write(thisLine+'\n')
             return True
         except Excetion as e:
-            self.__log('ERROR in write(self)')
+            self.__log('ERROR in write(self) : %s' % e)
             self.Errors.append(e)
             return False
+            
+    def grep(self, needle):
+        """
+        Search for a string like `grep string file`
+        No regex support here.
+        
+        If found, return the line. This makes it easier to .rm(line)
+        """
+        for line in self.contents:
+            if needle in line:
+                return line
+        return False
+
+    def egrep(self, pattern):
+        """
+        REGEX search for pattern in file
+        
+        If found, return the line. This makes it easier to .rm(line)
+        """
+        for line in self.contents:
+            if re.search(pattern, line):
+                return line
+        return False
+
+    def dump(self):
+        """
+        Print contents of file, useful when debugging.
+        """
+        for line in self.contents:
+            print unicode(line)
